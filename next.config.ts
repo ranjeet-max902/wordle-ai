11674eec24e96409a8fa8a9d05be56cdcb2885e2
ren0212322 <lazycrow712@gmail.com>
2025-12-10 03:11:47 +0530

    update  next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
   typescript: {
     ignoreBuildErrors: true,
   },
